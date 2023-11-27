819f6460c4cb342444aafafadfe1893cbe1fca6f
Lena Gagnon <lgagnon22@hotmail.com>
2023-11-27 16:50:24 -0500

    Added device, it's config basics and created the user model. I'm missing the device routes then

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'rentawheel', port: 3000 }
+  config.mailer_sender = 'no-reply@gmail.com'
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
