28c6889527fd01cc96b3e95372497de9136ac518
Rayane Bitchi <rayanebitchi@icloud.com>
2023-11-28 14:39:31 -0500

    seed cars from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,27 @@
 #   end
 
 # array of 5 colors
-# require 'open-uri'
-# require 'json'
-# colors = %w[red blue green yellow black]
-# # fecthing the data from cars API using open-uri and json to create 5 cars with model year marke and color
-# url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json'
-# makes_serialized = URI.open(url).read
-# makes = JSON.parse(makes_serialized)
-# # creating 5 users with email and password
-# 5.times do
-#     User.create!(email: Faker::Internet.email,
-#                  password: Faker::Internet.password)
-# end
-# makes['Results'].each do |make|
-#     Car.create!(model: Faker::Vehicle.model,
-#                 year: Faker::Vehicle.year,
-#                 color: colors.sample,
-#                 type: make['MakeName'],
-#                 user: User.all.sample)
-# end
-
 Car.destroy_all
-User.destroy_all
+require 'open-uri'
+require 'json'
+colors = %w[red blue green yellow black]
+# fecthing the data from cars API using open-uri and json to create 5 cars with model year marke and color
+url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json'
+makes_serialized = URI.open(url).read
+makes = JSON.parse(makes_serialized)
+# creating 5 users with email and password
+5.times do
+    User.create!(email: Faker::Internet.email,
+                 password: Faker::Internet.password)
+end
+makes['Results'].first(5).each do |make|
+    Car.create!(model: make['Model_Name'],
+                year: Faker::Vehicle.year,
+                color: colors.sample,
+                type: make['Make_Name'],
+                user: User.all.sample,
+                transmission: Faker::Vehicle.transmission)
+end
 
 
 
