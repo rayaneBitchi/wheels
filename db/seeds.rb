312cdc57131213d48737246dad5c7c7f3ffff936
Rayane Bitchi <rayanebitchi@icloud.com>
2023-11-28 14:20:26 -0500

    add faker gem to the Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,28 @@
 #   end
 
 # array of 5 colors
-colors = %w[red blue green yellow black]
-# fecthing the data from cars API using open-uri and json to create 5 cars with model year marke and color
-url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json'
-makes_serialized = open(url).read
-makes = JSON.parse(makes_serialized)
-# creating 5 users with email and password
-5.times do
-    User.create!(email: Faker::Internet.email,
-                 password: Faker::Internet.password)
-    end
-makes['Results'].each do |make|
-    Car.create!(model: Faker::Vehicle.model,
-                year: Faker::Vehicle.year,
-                color: colors.sample,
-                make: make['MakeName'],
-                user: User.all.sample)
-    end
+# require 'open-uri'
+# require 'json'
+# colors = %w[red blue green yellow black]
+# # fecthing the data from cars API using open-uri and json to create 5 cars with model year marke and color
+# url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json'
+# makes_serialized = URI.open(url).read
+# makes = JSON.parse(makes_serialized)
+# # creating 5 users with email and password
+# 5.times do
+#     User.create!(email: Faker::Internet.email,
+#                  password: Faker::Internet.password)
+# end
+# makes['Results'].each do |make|
+#     Car.create!(model: Faker::Vehicle.model,
+#                 year: Faker::Vehicle.year,
+#                 color: colors.sample,
+#                 type: make['MakeName'],
+#                 user: User.all.sample)
+# end
+
+Car.destroy_all
+User.destroy_all
 
 
 
