be2b0c7a46163815fbf1de01fb5b2ce6f6af5d5d
Lena Gagnon <lgagnon22@hotmail.com>
2023-11-29 15:11:58 -0500

    saving

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,4 +4,14 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  def new
+    @car = Car.new
+    @car.save
+  end
+
+  def create
+    raise
+    @car = Car.new()
+  end
 end
