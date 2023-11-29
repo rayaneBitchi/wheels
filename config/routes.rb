3253da1ce16f82585d218372b9d74a71fe271288
Rayane Bitchi <rayanebitchi@icloud.com>
2023-11-29 15:53:30 -0500

    Car grid created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :cars, only: [:show, :index]
-  devise_for :users
-  root to: "pages#home"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :cars, only: [:show, :index, :create, :new]
+  devise_for :users
+  root to: "pages#home"
 end
