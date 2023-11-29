a794220d22af691adc4d5739943596166d915eb3
Lennn375 <147986238+Lennn375@users.noreply.github.com>
2023-11-29 15:51:03 -0500

    Update cars_controller.rb
    you now need to be logged in to add a new car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:show]
 
   def show
     @car = Car.find(params[:id])
