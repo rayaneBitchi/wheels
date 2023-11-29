648562b92990afa6c7e581dfb6bd5591616e6cef
Lena Gagnon <lgagnon22@hotmail.com>
2023-11-29 14:34:03 -0500

    created a controleer dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :cars, only: [:show, :index]
+  resources :cars, only: [:show, :index, :new, :create]
   devise_for :users
   root to: "pages#home"
 
+  get "/profile", to: "dashboards#profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
