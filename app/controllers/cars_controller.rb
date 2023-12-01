5a0d50820ae93eefb67a26aab373620e21d86e57
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-01 13:50:41 -0500

    Delete <link_to> redirect to #index by default. I want it to redirect to profile_path. I will open a ticket after lunch...

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
-  before_action :set_car, only: [:show, :edit, :update, :delete]
+  before_action :set_car, only: [:show, :edit, :update, :destroy, :index]
 
   def show
     # @car = Car.find(params[:id])  #set in before action
@@ -27,7 +27,7 @@ class CarsController < ApplicationController
     redirect_to car_path(@car)
   end
 
-  def delete
+  def destroy
     # before_action defines @car
     @car.destroy
     redirect_to profile_path # status: :see_others
