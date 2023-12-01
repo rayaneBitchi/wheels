505a0f86ffee21b14803d5055dd3e6aa51516572
Romar Hamilton <romar.h0123@gmail.com>
2023-12-01 14:29:50 -0500

    changed made to views and models car

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,6 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
-  validates :model, presence: true
+  validates :model, presence: true, length: { minimum: 4 }
+  validates :year, presence: true, numericality: { in: 1960..2024 }
 end
